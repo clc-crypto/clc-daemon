2653e4b43112578427c0ce5ebc9cddc35b60353e
adamosnacho <adamoscom98@gmail.com>
2025-03-17 16:18:04 +0100

    Introduced boolean to control https use

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ import { loadLedger } from "./ledger";
 import Config from "./types/config";
 import register from "./endpoints/mining";
 import registerGeneral from "./endpoints/general";
-const https = require('https');
 
 const config: Config = JSON.parse(fs.readFileSync("config.json", 'utf8'));
 const app = express();
@@ -13,15 +12,24 @@ const app = express();
 // Enable CORS for all origins
 app.use(cors());
 
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/clc.ix.tc/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/clc.ix.tc/fullchain.pem', 'utf8');
-const credentials = { key: privateKey, cert: certificate };
+const useHttps: boolean = true;
 
 loadLedger(config);
 
 register(app, config);
 registerGeneral(app, config);
 
-https.createServer(credentials, app).listen(config.apiPort, () => {
-    console.log(`Server running on http://localhost:${config.apiPort}`);
-});
+if (useHttps) {
+    const https = require('https');
+
+    const privateKey = fs.readFileSync('/etc/letsencrypt/live/clc.ix.tc/privkey.pem', 'utf8');
+    const certificate = fs.readFileSync('/etc/letsencrypt/live/clc.ix.tc/fullchain.pem', 'utf8');
+    const credentials = { key: privateKey, cert: certificate };
+    https.createServer(credentials, app).listen(config.apiPort, () => {
+        console.log(`Server running on https://localhost:${config.apiPort}`);
+    });
+} else {
+    app.listen(config.apiPort, () => {
+        console.log(`Server running on http://localhost:${config.apiPort}`);
+    });
+}
