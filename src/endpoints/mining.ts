a21d65aa45f5819e5afb468ac40c5374c7b10de5
adamosnacho <adamoscom98@gmail.com>
2025-04-12 17:18:11 +0200

    Fixed timeout

--- src/endpoints/mining.ts
+++ src/endpoints/mining.ts
@@ -10,6 +10,7 @@ let CIRCULATION: number = 0;
 if (fs.existsSync("circulation.save")) CIRCULATION = parseFloat(fs.readFileSync("circulation.save", "utf-8"));
 let TARGET: number = 60000; // x ms / reward
 let lastFound = Date.now();
+let toID: null | NodeJS.Timeout = null; // Timeout ID
 
 function setUp(config: Config) {
     REWARD = config.reward;
@@ -18,9 +19,13 @@ function setUp(config: Config) {
     if (fs.existsSync("diff.save")) DIFF = fs.readFileSync("diff.save", "utf-8");
 
     console.log("Target timeout: " + config.targetTimeout);
+
+    toID = setTimeout(() => {
+        console.log("Took to long!");
+        cycle(config);
+    }, config.targetTimeout);
 }
 
-let toID: null | NodeJS.Timeout = null;
 function cycle(config: Config) {
     if (toID !== null) clearTimeout(toID);
     if (Date.now() - lastFound > TARGET && BigInt("0x" + DIFF) < BigInt("0x" + config.startingDiff)) DIFF = (BigInt("0x" + DIFF) + BigInt("0x" + config.adjust)).toString(16);
