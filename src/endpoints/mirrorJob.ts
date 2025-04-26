3ffddaf40ae105cdb56597c14c021d0915d40b13
adamosnacho <adamoscom98@gmail.com>
2025-04-26 12:08:31 +0200

    Fixed job queuing

--- src/endpoints/mirrorJob.ts
+++ src/endpoints/mirrorJob.ts
@@ -16,33 +16,42 @@ function setUp(cfg: Config) {
 let resolving: Array<string> = [];
 
 function resolveJobs() {
-    if (config === null) return;
-    for (const ip in JSON.parse(fs.readFileSync("mirrors.json", "utf-8"))) {
-        if (jobs[ip] && !resolving.includes(ip)) {
-            resolving.push(ip);
-            const job = jobs[ip].pop();
-            if (!job) continue;
-            if (jobs[ip].length === 0) delete jobs[ip];
+    try {
+        if (config === null) return;
+        for (const ip in JSON.parse(fs.readFileSync("mirrors.json", "utf-8"))) {
+            if (jobs[ip] && !resolving.includes(ip)) {
+                resolving.push(ip);
+                const job = jobs[ip].pop();
+                if (!job) continue;
+                if (jobs[ip].length === 0) delete jobs[ip];
 
-            betterFetch(job.endpoint, config.myIp === undefined ? "0.0.0.0" : config.myIp, job.data).then(res => {
-                resolving = resolving.filter(fip => fip !== ip);
-                console.log("Mirroring response (" + ip + "): " + res);
-            }).catch(e => {
-                resolving = resolving.filter(fip => fip !== ip);
-                console.log("Error mirroring: " + e.message);
-            });
+                betterFetch(job.endpoint, config.myIp === undefined ? "0.0.0.0" : config.myIp, job.data).then(res => {
+                    resolving = resolving.filter(fip => fip !== ip);
+                    console.log("Mirroring response (" + ip + "): " + res);
+                }).catch(e => {
+                    resolving = resolving.filter(fip => fip !== ip);
+                    console.log("Error mirroring: " + e.message);
+                });
+            }
         }
+    } catch (e: any) {
+        console.log(e.message);
     }
 }
 
 setInterval(resolveJobs, 100);
 
 function mirror(endpoint: string, data: any) {
-    for (const ip in JSON.parse(fs.readFileSync("mirrors.json", "utf-8"))) {
-        jobs[ip].push({
-            endpoint,
-            data
-        });
+    try {
+        for (const ip in JSON.parse(fs.readFileSync("mirrors.json", "utf-8"))) {
+            if (jobs[ip] === undefined) jobs[ip] = [];
+            jobs[ip].push({
+                endpoint,
+                data
+            });
+        }
+    } catch (e: any) {
+        console.log(e.message);
     }
 }
 
