b3e1ad4d9329617f866908233821ac2328ad6b59
adamosnacho <adamoscom98@gmail.com>
2025-04-26 12:40:42 +0200

    Fixed job queuing

--- src/endpoints/mirrorJob.ts
+++ src/endpoints/mirrorJob.ts
@@ -18,13 +18,12 @@ let resolving: Array<string> = [];
 function resolveJobs() {
     try {
         if (config === null) return;
-        for (const ip in JSON.parse(fs.readFileSync("mirrors.json", "utf-8"))) {
+        for (const ip of JSON.parse(fs.readFileSync("mirrors.json", "utf-8"))) {
             if (jobs[ip] && !resolving.includes(ip)) {
                 resolving.push(ip);
                 const job = jobs[ip].pop();
                 if (!job) continue;
                 if (jobs[ip].length === 0) delete jobs[ip];
-                console.log("Mirror to: " + ip + "/" + job.endpoint);
                 betterFetch(ip + "/" + job.endpoint, config.myIp === undefined ? "0.0.0.0" : config.myIp, job.data).then(res => {
                     resolving = resolving.filter(fip => fip !== ip);
                     console.log("Mirroring response (" + ip + "): " + res);
@@ -43,7 +42,7 @@ setInterval(resolveJobs, 100);
 
 function mirror(endpoint: string, data: any) {
     try {
-        for (const ip in JSON.parse(fs.readFileSync("mirrors.json", "utf-8"))) {
+        for (const ip of JSON.parse(fs.readFileSync("mirrors.json", "utf-8"))) {
             if (jobs[ip] === undefined) jobs[ip] = [];
             jobs[ip].push({
                 endpoint,
